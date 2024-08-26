96b105efdfdd432e609f3428679eacf6e1ed1857
Harsh Gupta <harshgupta88911@gmail.com>
2024-08-26 21:28:48 +0530

    uncommited changes

--- netflix-clone/tailwind.config.js
+++ netflix-clone/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"], // Ensure your paths are correct
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        netflix: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"], // Corrected syntax
+      },
+    },
   },
   plugins: [],
 };
