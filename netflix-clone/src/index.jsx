3f34655cf22bceb54a2838bb34c2549767947784
Harsh Gupta <harshgupta88911@gmail.com>
2024-08-26 21:26:46 +0530

    add index.jsx file for render all pages

--- netflix-clone/src/index.jsx
+++ netflix-clone/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./sass/app.css";
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignUpPage from "./SignUpPage";
 import SignInPage from "./SignInPage";
 import HomePage from "./HomePage";
