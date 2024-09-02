4abfeb7c9e84fcd1ed8749c879e718a6866b277b
Harsh Gupta <harshgupta88911@gmail.com>
2024-09-02 22:32:02 +0530

    update: footer height

--- netflix-clone/src/components/SignUpComponent/Footer.jsx
+++ netflix-clone/src/components/SignUpComponent/Footer.jsx
@@ -1,4 +1,3 @@
-import goForward from "../../assets/icons/rightArrow.png";
 import translate from "../../assets/icons/translate.png";
 
 const helpDetails = [
@@ -21,29 +20,7 @@ const helpDetails = [
 
 function Footer() {
   return (
-    <footer className="flex flex-col text-center h-[1000px] bg-black  py-5 px-5 lg:px-10">
-      <div className="text-left">
-        <p className="text-[15px] mt-3">
-          Ready to watch? Enter your email to create or restart your membership.
-        </p>
-        <form className="flex flex-col lg:flex-row text-left mt-4 ">
-          <input
-            type="text"
-            className="w-80 h-10 lg:h-[52px] rounded-[4px] lg:w-[720px] bg-[rgb(15,15,15,.3)] p-3 lg:p-3 border border-solid border-stone-400 placeholder:font-medium placeholder:text-gray-400"
-            placeholder="Email address"
-          />
-          <div className="cursor-pointer mt-4 lg:mt-0 bg-[rgb(229,9,20)] rounded-[4px] h-10 w-32 flex items-center  justify-between text-left lg:w-[195px] lg:h-[52px] p-3 lg:p-3 ml-0 lg:ml-4">
-            <button className="text-[16px] lg:text-[23px] lg:font-[600]">
-              Get Started
-            </button>
-            <img
-              src={goForward}
-              alt="goForward"
-              className="h-6 lg:h-[23px] lg:font-[600] ml-[10px] lg:ml-[15px]"
-            />
-          </div>
-        </form>
-      </div>
+    <footer className="flex flex-col text-center h-[800px] lg:h-[600px] bg-black  py-5 px-5 lg:px-10">
       <p className="space-x-2 text-left mt-6 ml-5">
         <span className="text-gray-400">Questions? Call</span>
         <span className="underline font-medium text-gray-400">
