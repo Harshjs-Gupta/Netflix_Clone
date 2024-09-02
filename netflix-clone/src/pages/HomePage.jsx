85f2ba404e687cf3f5423478aeff01e3904eed0c
Harsh Gupta <harshgupta88911@gmail.com>
2024-09-02 22:33:48 +0530

    added: home page

--- netflix-clone/src/pages/HomePage.jsx
+++ netflix-clone/src/pages/HomePage.jsx
@@ -1,12 +1,33 @@
-import HomeBanner from "../components/HomePageComponent/HomeBanner";
-import HomeHeader from "../components/HomePageComponent/HomeHeader";
+import { lazy, Suspense } from "react";
+// import HomeBanner from "../components/HomePageComponent/HomeBanner";
+// import HomeHeader from "../components/HomePageComponent/HomeHeader";
+// import MovieTemplates from "../components/HomePageComponent/MovieTemplates";
+import HomeSkeleton from "../components/HomePageSkeleton/HomeSkeleton";
+import Footer from "../components/SignUpComponent/Footer";
+// import SearchedMovieList from "../components/SearchMovieData/SearchedMovieList";
+import Movies from "../components/SearchMovieData/Movies";
+
+const HomeHeader = lazy(() =>
+  import("../components/HomePageComponent/HomeHeader")
+);
+const HomeBanner = lazy(() =>
+  import("../components/HomePageComponent/HomeBanner")
+);
+const MovieTemplates = lazy(() =>
+  import("../components/HomePageComponent/MovieTemplates")
+);
 
 function HomePage({ handleLogOut }) {
   return (
-    <div>
-      <HomeHeader handleLogOut={handleLogOut} />
-      <HomeBanner />
-    </div>
+    <>
+      <Suspense fallback={<HomeSkeleton />}>
+        <HomeHeader handleLogOut={handleLogOut} />
+        <HomeBanner />
+        <MovieTemplates />
+        <Footer />
+      </Suspense>
+      <Movies />
+    </>
   );
 }
 
