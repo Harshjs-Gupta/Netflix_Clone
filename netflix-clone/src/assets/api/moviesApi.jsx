6fce992627baec29388d154ccd0a71670df263a6
Harsh Gupta <harshgupta88911@gmail.com>
2024-09-02 23:48:55 +0530

    update: api call

--- netflix-clone/src/assets/api/moviesApi.jsx
+++ netflix-clone/src/assets/api/moviesApi.jsx
@@ -11,50 +11,40 @@ function useMovieData() {
   const API_KEY = "13d12680"; // Replace with your actual OMDB API key
 
   const getData = async function () {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    headers.append("Access-Control-Allow-Origin", "*");
-
     // Fetch comedy movies using the OMDB API
     const comedyRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=avenger`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=avenger`
     );
     const comedyMovies = await comedyRes.json();
     console.log(comedyMovies);
     setComedyMovieData(comedyMovies.Search || []);
 
     const classicRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=classic`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=classic`
     );
     const classicMovies = await classicRes.json();
     setClassicMovieData(classicMovies.Search || []);
 
     const horrorRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=horror`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=horror`
     );
     const horrorMovies = await horrorRes.json();
     setHorrorMovieData(horrorMovies.Search || []);
 
     const westernRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=western`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=western`
     );
     const westernMovies = await westernRes.json();
     setWesternMovieData(westernMovies.Search || []);
 
     const familyRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=family`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=family`
     );
     const familyMovies = await familyRes.json();
     setFamilyMovieData(familyMovies.Search || []);
 
     const mysteryRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=mystery`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=mystery`
     );
     const mysteryMovies = await mysteryRes.json();
     setMysteryMovieData(mysteryMovies.Search || []);
--- netflix-clone/src/assets/api/searchMovieByApi.jsx
+++ netflix-clone/src/assets/api/searchMovieByApi.jsx
@@ -6,15 +6,10 @@ function useSearchData() {
   const API_KEY = "13d12680";
 
   async function getMovieData() {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    headers.append("Access-Control-Allow-Origin", "*");
-
     if (!searchedMovie) return; // Only fetch data if there is a search term
 
     const searchMovieRes = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchedMovie}`,
-      { headers: headers }
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchedMovie}`
     );
     const movies = await searchMovieRes.json();
     setComingMovieData(movies.Search || []); // Set the movie data or an empty array if no results
