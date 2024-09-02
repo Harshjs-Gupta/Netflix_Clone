f12911b80623cfb1e878dc79e6c49ba1cf70bf51
Harsh Gupta <harshgupta88911@gmail.com>
2024-09-02 22:37:26 +0530

    added: tailwind config

--- netflix-clone/src/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <div className="text-black">Home Page</div>;
-}
-
-export default HomePage;
--- netflix-clone/tailwind.config.js
+++ netflix-clone/tailwind.config.js
@@ -5,9 +5,40 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        netflix: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"], // Corrected syntax
+        netflix: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"],
+        basker: ["'BaskerFont'", "san-serif"],
+      },
+      boxShadow: {
+        "inner-bottom": "inset 0 -10px 10px rgb(0, 0, 0)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".custom-scrollbar::-webkit-scrollbar": {
+            width: "4px",
+            height: "2px",
+          },
+          ".custom-scrollbar::-webkit-scrollbar-track": {
+            background: "transparent",
+          },
+          ".custom-scrollbar::-webkit-scrollbar-thumb": {
+            "background-color": "#888",
+            "border-radius": "10px",
+            border: "2px solid #f1f1f1",
+          },
+          ".custom-scrollbar::-webkit-scrollbar-thumb:hover": {
+            background: "transparent",
+          },
+          ".custom-scrollbar": {
+            "scrollbar-width": "thin",
+            "scrollbar-color": "black transparent",
+          },
+        },
+        ["responsive"]
+      );
+    },
+  ],
 };
