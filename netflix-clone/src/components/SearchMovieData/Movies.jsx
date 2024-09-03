4aa9ff2f5bf1678df5ff10714794413957d76e27
Harsh Gupta <harshgupta88911@gmail.com>
2024-09-03 12:36:49 +0530

    update: movie visiblity function

--- netflix-clone/src/components/SearchMovieData/Movies.jsx
+++ netflix-clone/src/components/SearchMovieData/Movies.jsx
@@ -1,35 +1,29 @@
 import HomeHeader from "../HomePageComponent/HomeHeader";
 
 function Movies({ comingMovieData }) {
-  const { showMovies } = HomeHeader();
-
   return (
-    <>
-      {showMovies && (
-        <div className="absolute z-5 top-[55px] right-7 flex h-[350px] w-[200px]  lg:h-[420px] lg:w-[300px] bg-black py-3 px-2">
-          <div className="flex flex-col gap-4 hover:overflow-y-scroll overflow-hidden custom-scrollbar">
-            {comingMovieData &&
-              comingMovieData.map((movie) => (
-                <div className="flex gap-2 z-50">
-                  <img
-                    src={movie.Poster}
-                    alt="movie"
-                    className="cursor-pointer h-[80px] w-[50px] lg:h-[140px] lg:w-[90px] object-cover ml-4"
-                  />
-                  <div className="flex flex-col">
-                    <span className="font-medium ml-2 text-sm lg:text-[18px]">
-                      {movie.Title}
-                    </span>
-                    <span className="font-medium ml-2 text-sm lg:text-[18px]">
-                      {movie.Year}
-                    </span>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-      )}
-    </>
+    <div className="absolute z-5 top-[55px] right-7 flex h-[350px] w-[200px]  lg:h-[420px] lg:w-[300px] bg-black py-3 px-2">
+      <div className="flex flex-col gap-4 hover:overflow-y-scroll overflow-hidden custom-scrollbar">
+        {comingMovieData &&
+          comingMovieData.map((movie) => (
+            <div className="flex gap-2 z-50">
+              <img
+                src={movie.Poster}
+                alt="movie"
+                className="cursor-pointer h-[80px] w-[50px] lg:h-[140px] lg:w-[90px] object-cover ml-4"
+              />
+              <div className="flex flex-col">
+                <span className="font-medium ml-2 text-sm lg:text-[18px]">
+                  {movie.Title}
+                </span>
+                <span className="font-medium ml-2 text-sm lg:text-[18px]">
+                  {movie.Year}
+                </span>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 }
 
