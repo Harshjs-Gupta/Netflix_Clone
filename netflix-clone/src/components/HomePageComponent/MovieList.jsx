d41472c344cdabc9490101b1096c93d485aa4e23
HarshGupta <harshgupta88911@gmail.com>
2025-11-09 22:59:38 +0530

    update: add link for better routes

--- netflix-clone/src/components/HomePageComponent/MovieList.jsx
+++ netflix-clone/src/components/HomePageComponent/MovieList.jsx
@@ -1,12 +1,20 @@
-function MovieList({ id, imgUrl, movieName }) {
+import React from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+function MovieList({ movieId, imgUrl, movieName }) {
+  const { id } = useParams();
   return (
-    <div className="flex-none ml-2">
+    <Link to={`/movie-player/${movieId}`} className="flex-none ml-2" key={id}>
       <img
         src={imgUrl}
         alt="movie"
         className="cursor-pointer h-36 lg:h-[180px] w-[130px] object-cover"
       />
-    </div>
+      <p className="mt-2 text-sm text-center w-[130px] overflow-hidden text-ellipsis whitespace-nowrap">
+        {movieName}
+      </p>
+    </Link>
   );
 }
 
