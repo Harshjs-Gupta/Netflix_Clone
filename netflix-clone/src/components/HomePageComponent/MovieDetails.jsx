1c0b1080735e79405da8fa0722c6321b7db1d392
HarshGupta <harshgupta88911@gmail.com>
2025-11-09 22:58:59 +0530

    update: its component structure :rocket:

--- netflix-clone/src/components/HomePageComponent/MovieDetails.jsx
+++ netflix-clone/src/components/HomePageComponent/MovieDetails.jsx
@@ -1,17 +1,29 @@
+import { Link } from "react-router-dom";
 import MovieList from "./MovieList";
+import { useParams } from "react-router-dom";
 
 function MovieDetails({ movieType, movieTitle }) {
+  const { id } = useParams();
+  // console.log(movieType);
   return (
     <>
-      <div className="ml-3 pt-3 text-xl font-semibold">{movieTitle}</div>
-      <div className="ml-4 mt-2 flex gap-2 overflow-x-hidden hover:overflow-x-auto whitespace-nowrap custom-scrollbar">
+      <div className="pt-3 ml-3 text-xl font-semibold">{movieTitle}</div>
+      <div className="flex gap-2 mt-2 ml-4 overflow-x-hidden hover:overflow-x-auto whitespace-nowrap custom-scrollbar">
         {movieType?.map((movie) => (
-          <MovieList
-            id={movie.imdbId}
-            imgUrl={movie.Poster || movie.posterUrl}
-            movieName={movie.Title || movie.title}
-            key={movie.imdbID}
-          />
+          <Link
+            to={`/movie-player/${movie.id}`}
+            className="flex-none ml-2"
+            key={movie.id}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title || movie.name}
+              className="cursor-pointer h-36 lg:h-[180px] w-[130px] object-cover"
+            />
+            <p className="mt-2 text-sm text-center w-[130px] overflow-hidden text-ellipsis whitespace-nowrap">
+              {movie.title || movie.name}
+            </p>
+          </Link>
         ))}
       </div>
     </>
